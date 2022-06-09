605715d6b688ff300f03d64a39945d4a6efa4c26
sina-lib <mszandian@aut.ac.ir>
2022-06-09 10:49:16 -0400

    new codes

--- main.php
+++ main.php
@@ -2,16 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Job Hub</title>
-        <link rel="stylesheet" href="main.css" />
+        <title>Job Magnifier</title>
+        <link rel="stylesheet" href="main.css?<?php echo time(); ?>" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>   
     </head>
     <body>
         <div class="container">
             <header>
-                <img id="logo" src="jobhub.png" height="50px">
+                <img id="logo" src="JobMag_Icon.png" width="20%" height="160px"  >
+                <img id="TopPicture" src="head_pic.jpeg" width="78%" height="160px" >
             </header>
+            <hr>
+            <h1> List of Jobs: </h1>
+            <hr>
             <nav>
                 <hr>
                 <form name="RefineForm" Action="" Method="GET">
@@ -29,20 +33,22 @@
                     </div>
                     <input type="button" name="RefineFormSubmit" Value="GO!" onClick="submitRefine(this.form)" id="RefineFormSubmit"/>
                 </form>
-                <hr>
-                <p>3 results</p>
+                <hr> 
+                <p id="TheIndexNumber">  results</p>
             </nav>
             <main>
                 <div class='anchor' id='anchor'></div>
             </main>
             <footer>
-                <img src="huaji.gif" class='center'>
+                 <img src="huaji.gif" class='center'>
+                <p id="FooterP"> Copyright 2022 </p>
             </footer>
         </div>
         <script>
             var index = 0;
             var mustHaveInTitle = "";
-            var orderBy = "Default"
+            var orderBy = "Default";
+            var searchedNumber_node = document.getElementById("TheIndexNumber");
 
             load_more();
 
@@ -80,13 +86,16 @@
             }
 
             function load_more() {
+                var indexP1 = index + 1;
+                searchedNumber_node.innerHTML = indexP1 + " page result ";
                 $.ajax({
                     url: `loadmore.php?${build_url_vars()}`,
                     type: "get",
                     beforeSend: function (){
-                        $('.ajax-loader').show();
+                        $('.ajax-loader').show(); 
                     },
                     success: function (data) {
+                        console.log("appending...");
                         $(".anchor").append(data);
                     }
                 });
